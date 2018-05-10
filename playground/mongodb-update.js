727a536cf72a1ed1cf6393e72160238c0e05ef5e
anna_f <annafoldvari@gmx.co.uk>
2018-05-10 17:46:14 +0100

    Add POST /todos route and refactor moongose

--- playground/mongodb-update.js
+++ playground/mongodb-update.js
@@ -8,16 +8,18 @@ MongoClient.connect('mongodb://localhost:27017/TodoApp', (err, db) => {
   }
   console.log("Connected to MongoDB server.");
 
-db.collection('Todos').findOneAndUpdate({
-    _id: new ObjectID("5af1b8dcdd24ca0e71c92e29")
-}, {
-    $set: {
-       completed: true
-    }
-}, {
-    returnOriginal: false
-}).then((result) => {
-    console.log(result);
-});
+// db.collection('Todos').findOneAndUpdate({
+//     _id: new ObjectID("5af1b8dcdd24ca0e71c92e29")
+// }, {
+//     $set: {
+//        completed: true
+//     }
+// }, {
+//     returnOriginal: false
+// }).then((result) => {
+//     console.log(result);
+// });
+
+db.collection('Users')
   //db.close();
 });
